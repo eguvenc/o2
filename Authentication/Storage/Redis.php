eefc3cf71a1c45f8c34ae94dafc575ad043be84e
eguvenc <eguvenc@gmail.com>
2015-01-31 11:50:59 +0200

    updates

--- Authentication/Storage/Redis.php
+++ Authentication/Storage/Redis.php
@@ -43,10 +43,10 @@ Class Redis extends AbstractStorage
         $this->session = $this->c->load('session');
 
         $this->cache = $this->c->load(
-            'service provider '.$this->config['auth']['provider']['name'], 
+            'service provider '.$this->config['cache']['provider']['name'], 
             [
-                'driver' => $this->config['auth']['provider']['driver'], 
-                'serializer' => $this->config['auth']['provider']['serializer']
+                'driver' => $this->config['cache']['provider']['driver'], 
+                'serializer' => $this->config['cache']['provider']['serializer']
             ]
         );
     }
