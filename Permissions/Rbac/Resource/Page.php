d78f9de3c41ca796c8475f192d06a7f813af3518
artase <ihsancaglayan@gmail.com>
2015-02-19 09:47:19 +0200

    update

--- Permissions/Rbac/Resource/Page.php
+++ Permissions/Rbac/Resource/Page.php
@@ -108,6 +108,32 @@ Class Page implements ArrayAccess
         }
         return $resultArray;
     }
+
+    /**
+     * Get permission
+     * 
+     * @param mix $opName     operations ( view,update,delete,insert,save )
+     * @param int $expiration expiration time
+     * 
+     * @return boolean
+     */
+    public function getAllPermissions($expiration = 7200)
+    {
+        // $opName      = Utils::arrayConvert($opName);
+        // $key         = User::CACHE_HAS_PAGE_PERMISSION . $this->c['rbac.user']->getId() .':'. Utils::hash($this->c['rbac.resource']->getId()) .':'. Utils::hash($opName);
+        // $resultArray = $this->c['rbac.user']->cache->get($key);
+        $resultArray = false;
+        
+        if ($resultArray === false) { // If not exist in the cache
+            $queryResultArray = $this->c['model.user']->getPagePermissionsSqlQuery();  // do sql query
+            $resultArray      = ($queryResultArray == false) ? 'empty' : $queryResultArray;
+            // $this->c['rbac.user']->cache->set($key, $resultArray, $expiration);
+        }
+        if ($resultArray == 'empty') {
+            return false;
+        }
+        return $resultArray;
+    }
 }
 
 
